59ffb91f4ced8da0e2b617c03050ca2361ff5702
shashika <shashika.shehan.kumarasinghe99@gmail.com>
2024-03-06 22:12:58 +0530

    update

--- home.php
+++ home.php
@@ -7,13 +7,13 @@
     </head>
 
     <script>
-    fetch('header.html')
+    fetch('header_footer/header2.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header').innerHTML = data;
         })
 
-    fetch('footer.html')
+    fetch('header_footer/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer').innerHTML = data;
@@ -23,7 +23,7 @@
     <body>
         <div id="header"></div>
 
-
+        <h1>home page</h1>
 
 
         <div id="footer"></div>
